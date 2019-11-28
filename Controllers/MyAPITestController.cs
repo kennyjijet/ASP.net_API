ddebe29a2ee57ef9551c19601e2e1aa4520a3e2e
Justin Jetwiriyanon <jittarinjetwiriyanon@justins-MacBook-Air.local>
2019-11-28 18:27:23 +1300

    first commit

---
+++ Controllers/MyAPITestController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyAPITestController : ControllerBase
+    {
+
+        private readonly ILogger<MyAPITestController> _logger;
+
+        public MyAPITestController(ILogger<MyAPITestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<MyAPITest> Get()
+        {
+            /* 
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MyAPITest
+            {
+                MyValue = "TEST"
+            })
+            .ToArray();
+            */
+            MyAPITest myAPITest = new MyAPITest();
+            myAPITest.MyValue = "TEST";
+            IEnumerable<MyAPITest> IElist = new IEnumerable<MyAPITest>();
+            IElist.Append(myAPITest);
+
+            return IElist;
+        }
+    }
+}
---
+++ MyAPITest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyAPI
+{
+    public class MyAPITest
+    {
+        public string MyValue { get; set; }
+
+    }
+}
